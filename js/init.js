d1703b12e6e1147e9651b20afa5106816417bf1f
Madhu Krishnan <MadhuV.Krishnan@gmail.com>
2014-11-13 12:48:30 -0800

    Officer emails. Fixed scaling.

--- js/init.js
+++ js/init.js
@@ -9,8 +9,8 @@
   skel.init({
     reset: 'full',
   breakpoints: {
-    global:		{ range: '*', href: 'css/style.css', containers: '60em', grid: { gutters: { vertical: '2em', horizontal: 0 } } },
-  wide:		{ range: '-2560', href: 'css/style-wide.css' },
+    global:		{ range: '*', href: 'css/style.css', containers: '70%', grid: { gutters: { vertical: '2em', horizontal: 0 } } },
+  wide:		{ range: '-9999', href: 'css/style-wide.css' },
   normal:		{ range: '-1280', href: 'css/style-normal.css', grid: { gutters: { vertical: '1.5em' } }, viewport: { scalable: false } },
   narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '90%' },
   narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: 1 } },
