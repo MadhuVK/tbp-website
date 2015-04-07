7a69d1770a91f0527a2c8f75c82333b1a997140c
Madhu <MadhuV.Krishnan@gmail.com>
2015-04-06 23:37:03 -0700

    Mindset stuff

---
+++ florence.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <?php include('common/head.php') ?>
+</head>
+
+<body>
+
+  <!-- Header -->
+  <header id="header">
+    <?php include('common/header.php') ?>
+  </header>
+
+  <!-- Main -->
+  <section id="main" class="container">
+    <header>
+     <h2>Florence Elementary Outreach</h2>
+   </header>
+   <div class="box features">
+     <div class="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th width="20%">Module</th>
+            <th width="40%">Summary</th>
+            <th width="10%">Module Type</th>
+            <th width="30%">Materials Needed</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><b>Cardiovascular System (3/13/2015)</b></td>
+            <td class="node">            
+The goal is for students to understand how the body responds to physical stress so that they can get a deeper insight into human physiology.
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+              <td>Marshmallow Catapults (11/7/2014)</td>
+              <td class="node">            
+                Students learn the effects of gravity by building marshmallow launching catapults              
+              </td>
+            <td>Physics</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </div>
+          </tr>
+          <tr>
+            <td>Marble Maze (11/14/2014)</td>
+            <td class="node">            
+            </td>
+            <td>Physics</td>
+            <td class="node">
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Straw Bridge (11/21/2014)</td>
+            <td class="node">            
+Students work in groups to build the longest bridge made of straw between two chairs. This exercise focused on teamwork, strategic building, allocation of resources, and a basic understanding of mechanical integrity.
+            </td>
+            <td>Structural Engineering</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>M&amp;M Torque (12/05/2014)</td>
+            <td class="node">            
+Students focus on gravity, mass, and torque using M&amp;Ms.
+            </td>
+            <td>Physics</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Ice Cream (12/12/2014)</td>
+            <td class="node">            
+Students focus on chemistry and colligative properties by making ice cream in a bag.
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Glue Rockets (1/23/2015)</td>
+            <td class="node">            
+Students learn chemistry and pressure by making rockets with alka-seltzer and glue sticks.
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Dots Tower (1/30/2015)</td>
+            <td class="node">            
+Students focus on understanding mechanical structures, management of resources, teamwork, and gravity by engineering their effect on towers.
+            </td>
+            <td>Structural Engineering</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Ooblec non-Newtonian Object (2/06/2015)</td>
+            <td class="node">            
+Students interact with a non-newtonian fluid and consider molecular processes.  
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Lava Lamp (2/27/2015)</td>
+            <td class="node">            
+Students learn about density and phobicity.   
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>The Senses (3/6/2015)</td>
+            <td class="node">            
+Students explore the senses of the human body, particularly taste and smell.
+            </td>
+            <td>Life Sciences</td>
+            <td class="node">            
+             <span class="node-title">
+                <a href="javascript:void(0)">Expand</a>
+              </span>
+              <div class="node-contents">
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</section>
+
+  <footer>
+    <?php include('common/footer.php') ?>
+  </footer>
+</body>
+</html>
