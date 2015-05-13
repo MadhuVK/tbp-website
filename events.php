76b5e2b17be8e1306f3ee0ad4307110881626505
Madhu <MadhuV.Krishnan@gmail.com>
2015-05-12 19:04:15 -0700

    Week 7 (Late)

--- events.php
+++ events.php
@@ -20,42 +20,11 @@
     </header>
     <section class="box">
     <section class="major special">
-    <h2><u>Week 6 Announcements</u></h2>
+    <h2><u>Week 7 Announcements</u></h2>
     </section>
     <ol>
       <li>
-        <b><a href="elections">2015-2016 Officer Elections</a></b> <br />
-        Interested in joining Tau Beta Pi’s Officer Team for the 
-        2015-2016 year? Being a part of the Tau Beta Pi Officer Team 
-        is a fantastic way to gain leadership experience and help shape 
-        the TBP Community at UCSD. Elections will take place after 
-        Initiation, so even if you are currently an Initiate, it is 
-        still possible for you to run for a position. 
-        Check out the list of available positions and their descriptions 
-        <a href="elections">here</a>. Also be sure to come to the open officer events where you can learn more about being an Officer of TBP. 
-      </li>
-      <br />
-      <li>
-        <b><a href="https://docs.google.com/spreadsheets/d/1dSAtaa_xJRLJbOFhcin4cr7EumNL1XjfAjvn7qQf9lg/edit#gid=0">Triton 3D Make-A-Thon Volunteering</a></b> <br/>
-        A new student org called Triton 3D is host a Make-athon on May 16th-17th. This is a two-day competition where teams of students will collaborate, design, and 3D-print entries to fulfill a specific design goal. They are currently in need of volunteers to help out during the event! Sign up if you are interested. 
-        If you have any questions, please contact <a href="mailto:c1yan@ucsd.edu">Crystal</a>.
-        <br />
-        Sign up <a href="https://docs.google.com/spreadsheets/d/1dSAtaa_xJRLJbOFhcin4cr7EumNL1XjfAjvn7qQf9lg/edit#gid=0">here</a>.
-      </li>
-      <br />
-      <li>
-        <b> Faculty of the Year Nominations </b> <br />
-        Each year Tau Beta Pi recognizes a Faculty of the Year 
-        who has positively influenced and developed the lives of 
-        young engineers. If you know of a professor who is deserving 
-        of this honor, please nominate them for this award. 
-        The award will be presented at 
-        Tau Beta Pi's Annual End-of-the-Year Banquet on the e
-        vening of May 29th. You may make up to three nominations. 
-        Nominations are <b>due by 11:59pm on Thursday, May 7th</b>. 
-        Voting will take place right after officer elections. 
-        If you have someone in mind you would like to nominate, 
-        please fill out the nomination form <a href="http://goo.gl/forms/GHZlyx2YyH">here </a>.
+        <b><a href="images/officers/officers_1516.png">Congratulations to our new Officers!</a></b> <br />
       </li>
       <br />
       <li>
@@ -80,45 +49,24 @@ Look no further! Distinguish yourself when you walk the stage with an official T
         And then once you've written that and after the officers have approved it, you can bring your
         point sheet to the next event you attend and have an officer sign your form then.
       </li>
-      <br />
-      <li>
-        <b>Tau Beta Pi National Scholarship Applications</b> <br />
-        Current Tau Beta Pi members or initiates that will be members by June 1st 
-        that are planning to take at least two quarters of full-time undergraduate 
-        study in engineering on campus in 2015-16 are eligible to apply for 
-        Tau Beta Pi's scholarships. There is $1,000 to $2,000 <a href="http://www.tbp.org/scholarships.cfm">Scholarship</a>
-        for the 2015-16 academic year. Applications are due by May 1st, 2015. Register <a href="http://www.tbp.org/TBPAPPS">here</a> to receive an application. 
-      </li>
     </ol>
 
     </section>
     <div class="row">
       <div class="4u">
         <section class="box align-center" id="upcoming">
-        <h3><b>Week 6</b></h3>
-        <h3>Open Officer Meeting</h3>
-        <p>
-        Event Type: Academic<br>
-        Mon May 4 | 8:00PM - 9:00PM<br>
-        Location: CSE 2217
-        <hr />
+        <h3><b>Week 7</b></h3>
         <h3>Intramural Dodgeball</h3>
         <p>
         Event Type: Social<br>
-        Tue May 5 | 7:00PM - 8:00PM <br>
+        Tue May 12 | 6:00PM - 7:00PM <br>
         Location: RIMAC</a>
         </p>
-        <hr />
-        <h3>TBP Officer Elections</h3>
-        <p>
-        Event Type: Academic/Social<br>
-        Thu May 7 | 7:00PM - 9:00PM<br>
-        Location: TBD
         <hr />
         <h3>Florence Elementary</h3>
         <p>
         Event Type: Outreach<br>
-        Fri May 8 | 8:30AM - 10:30AM <br>
+        Fri May 15 | 8:30AM - 10:30AM <br>
         Location: Contact <a href="mailto:catrinh@ucsd.edu">Cindy Trinh</a>
         </p>
         <hr />
@@ -129,12 +77,18 @@ Look no further! Distinguish yourself when you walk the stage with an official T
         Location: Price Center Game Room
         </p>
         <hr />
-        <h3><a href="https://www.facebook.com/events/652962701505513/">Tau Beta Potluck and Movie</a></h3>
+        <h3>Officer Retreat</h3>
         <p>
-        Event Type: Social
-        Sun May 10 | 6:30PM - 9:30PM<br>
-        Location: TBA
+        Event Type: Social <br>
+        Fri May 15 - Sun May 17  <br>
+        Lake Gregory
         <hr />
+        <h3><a href="https://docs.google.com/spreadsheets/d/1dSAtaa_xJRLJbOFhcin4cr7EumNL1XjfAjvn7qQf9lg/edit#gid=0">Triton 3D Make-A-Thon Volunteering</a></h3>
+        <p>
+        Event Type: Community Service 
+        Sat May 16 - Sun May 17 <br> 
+        Location: Contact <a href="mailto:c1yan@ucsd.edu">Crystal</a>
+        </p>
         </section>
     </div>
     <div class="8u">
--- index.php
+++ index.php
@@ -17,8 +17,7 @@
     <h2>Tau Beta Pi, California Psi</h2>
     <p>The Premier Engineering Honor Society at UCSD</p>
     <ul class="actions">
-      <li><a href="events" class="button special"><b>Week 6 Announcements</b></a></li>
-      <li><a href="elections" class="button special"><b>2015-2016 Officer Elections</b></a></li>
+      <li><a href="events" class="button special"><b>Week 7 Announcements</b></a></li>
     </ul>
   </section>
 
